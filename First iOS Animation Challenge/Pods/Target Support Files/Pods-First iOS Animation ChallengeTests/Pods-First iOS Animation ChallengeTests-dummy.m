ec5416d2fa0db2e0c8935e02464c3e8995b7aed1
Jing Gao <tracygj1990@gmail.com>
2019-01-27 16:57:23 +1100

    pod init

---
+++ First iOS Animation Challenge/Pods/Target Support Files/Pods-First iOS Animation ChallengeTests/Pods-First iOS Animation ChallengeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_First_iOS_Animation_ChallengeTests : NSObject
+@end
+@implementation PodsDummy_Pods_First_iOS_Animation_ChallengeTests
+@end
---
+++ First iOS Animation Challenge/Pods/Target Support Files/Pods-First iOS Animation ChallengeUITests/Pods-First iOS Animation ChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_First_iOS_Animation_ChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_First_iOS_Animation_ChallengeUITests
+@end
