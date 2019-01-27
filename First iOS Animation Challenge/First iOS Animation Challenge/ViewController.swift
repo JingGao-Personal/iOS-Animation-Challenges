4f3ac7a624d3ed188793c1dbd873f709847263c0
Jing Gao <tracygj1990@gmail.com>
2019-01-27 16:56:33 +1100

    first commit

---
+++ First iOS Animation Challenge/First iOS Animation Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First iOS Animation Challenge
+//
+//  Created by Jing Gao on 27/1/19.
+//  Copyright © 2019 Jing and Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ First iOS Animation Challenge/First iOS Animation ChallengeTests/First_iOS_Animation_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  First_iOS_Animation_ChallengeTests.swift
+//  First iOS Animation ChallengeTests
+//
+//  Created by Jing Gao on 27/1/19.
+//  Copyright © 2019 Jing and Yu. All rights reserved.
+//
+
+import XCTest
+@testable import First_iOS_Animation_Challenge
+
+class First_iOS_Animation_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
